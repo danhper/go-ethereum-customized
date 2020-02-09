5213c8755713fba94f5ac2f78b1053c09b00439d
Daniel Perez <daniel@perez.sh>
2020-02-09 13:47:48 +0000

    Rename python package and add csv file with holders

--- maker-simulator/setup.py
+++ maker-simulator/setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="feed_syncer",
+    name="maker_simulator",
     packages=find_packages(),
     install_requires=[
         # "web3", use forked version
     ],
+    script=["./bin/maker-simulator"],
     extras_require={
         "dev": [
             "pylint",
