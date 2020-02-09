2dba58502ac10ca265e678e83ac8ad888500b1b3
Daniel Perez <daniel@perez.sh>
2020-02-09 15:26:08 +0000

    Fix event fetcher

--- maker-simulator/maker_simulator/etherscan_events_fetcher.py
+++ maker-simulator/maker_simulator/etherscan_events_fetcher.py
@@ -12,22 +12,27 @@ MAX_RETRIES = 3
 
 
 def fetch_events(contract_address, fout, max_to_fetch=100_000):
-    page = 1
+    session = requests.Session()
+
     params = dict(
         module="account",
         action="tokentx",
         contractaddress=contract_address,
-        page=page,
         offset=PER_PAGE,
         sort="desc",
         apikey=settings.ETHERSCAN_API_KEY,
     )
-    session = requests.Session()
+
+    page = 1
     fetched = 0
     retry_count = 0
+
     while fetched < max_to_fetch and retry_count <= MAX_RETRIES:
         logging.info("%s/%s", fetched, max_to_fetch)
+
+        params["page"] = page
         res = session.get(settings.ETHERSCAN_API_URL, params=params)
+
         payload = res.json()
         if payload["status"] != "1":
             time.sleep(2 ** retry_count)
@@ -38,8 +43,13 @@ def fetch_events(contract_address, fout, max_to_fetch=100_000):
             continue
 
         retry_count = 0
+
+        if not payload["result"]:
+            break
+
         for event in payload["result"]:
             json.dump(event, fout)
             print(file=fout)
+
         fetched += len(payload["result"])
         page += 1
